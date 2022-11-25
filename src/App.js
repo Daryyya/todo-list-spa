3c68b1f5012833aa03744860ccd4c815a191ccd2
Daryyya <darya9841402670@gmail.com>
2022-11-25 12:24:36 +0300

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Task from "./pages/Task";
 function App() {
   return (
-    <></>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/tasks" element={<Task/>}/>
+    </Routes>
   );
 }
 
